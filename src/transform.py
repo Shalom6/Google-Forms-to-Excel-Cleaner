c41609a60919af2517fde4b7c19c021636430632
Shalom Oyewusi <damolaso@hotmail.com>
2025-09-03 01:19:07 -0600

    tranform.py is fully implemented

---
+++ src/transform.py
@@ -0,0 +1,10 @@
+def transform(df):
+    df.sort_values(by=["timestamp"], ascending=True)
+    df = df[["timestamp","name", "email", "address", "phone_number"]]
+    df.loc[:, "name"] = df["name"].str.title()
+    df.loc[:, "email"] = df["email"].str.strip()
+    df.loc[:, "address"] = df["address"].str.strip()
+    df.loc[:, "phone_number"] = df["phone_number"].str.strip()
+    df = df[["timestamp", "name", "email", "address", "phone_number"]].copy()
+
+    return df

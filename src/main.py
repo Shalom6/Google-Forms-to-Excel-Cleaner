56f6a0eb64f04a87b721c94fe0e425c5958de05f
Shalom Oyewusi <damolaso@hotmail.com>
2025-09-03 00:09:44 -0600

    main.py is functional and testing works

--- src/main.py
+++ src/main.py
@@ -1,25 +1,7 @@
-from googleapiclient.discovery import build
-from google.oauth2 import service_account
+from fetch_forms import fetch_form_responses
 
-# Path to service account credentials
-SERVICE_ACCOUNT_FILE = "credentials/databridge-service.json"
+if __name__ == "__main__":
+    sheet_id = "1nkrAqoIBc6Ze841PgVlElOhpHnGbw8hL9qpGM-BSUkQ"
+    df = fetch_form_responses(sheet_id)
+    print(df.head())
 
-# Scopes = permissions for Docs + Drive
-SCOPES = [
-    'https://www.googleapis.com/auth/documents.readonly',
-    'https://www.googleapis.com/auth/drive.readonly'
-]
-
-# Authenticate
-creds = service_account.Credentials.from_service_account_file(
-    SERVICE_ACCOUNT_FILE, scopes=SCOPES
-)
-
-# Build Docs API service
-docs_service = build('docs', 'v1', credentials=creds)
-
-# Replace with your Doc ID (from its URL)
-DOCUMENT_ID = "YOUR_GOOGLE_DOC_ID_HERE"
-
-doc = docs_service.documents().get(documentId=DOCUMENT_ID).execute()
-print("Title:", doc.get('title'))

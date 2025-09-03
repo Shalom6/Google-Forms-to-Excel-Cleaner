fd9cf6ee3b1bd9a6c67624481f631ffd7108ac22
Shalom Oyewusi <damolaso@hotmail.com>
2025-09-03 01:55:35 -0600

    testing for fetch_forms.py, clean_data.py, and transform.py are functional

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from fetch_forms import fetch_form_responses
 from clean_data import clean_responses
 from transform import transform
+from export_excel import export_to_excel
 if __name__ == "__main__":
     sheet_id = "1nkrAqoIBc6Ze841PgVlElOhpHnGbw8hL9qpGM-BSUkQ"
     df = fetch_form_responses(sheet_id)
@@ -16,3 +17,10 @@ df = fetch_form_responses(sheet_id)
 cleaned_df = clean_responses(df)
 transformed_df = transform(cleaned_df)
 print(transformed_df.head())
+
+df = fetch_form_responses(sheet_id)
+cleaned_df = clean_responses(df)
+transformed_df = transform(cleaned_df)
+
+export_to_excel(transformed_df, "form_responses.xlsx")
+print("Excel file created successfully!")

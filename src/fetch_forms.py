5aa31be9f51ef466c7e849cb588ef19cadd58347
Shalom Oyewusi <damolaso@hotmail.com>
2025-09-03 00:09:19 -0600

    fetch_forms.py is fully implemented

--- src/fetch_forms.py
+++ src/fetch_forms.py
@@ -1,9 +1,29 @@
-from google.oauth2.service_account import credentials
+from google.oauth2.service_account import Credentials
 import gspread
 import pandas as pd
-print("Imports successful!")
 
+# print("Imports successful!")
 
-#def fetch_form_responses(sheet_id):
+def fetch_form_responses(sheet_id):
+    # load JSON file → create credentials object → set scopes
+    creds = Credentials.from_service_account_file(
+        "../credentials/forms-excel-service.json",
+        scopes=["https://www.googleapis.com/auth/spreadsheets.readonly"]  # Fill in the Sheets API scope here
+    )
 
-    #pass
+    #using gspread to authorize with credientials
+    client = gspread.authorize(creds)
+
+    sheet_id = "1nkrAqoIBc6Ze841PgVlElOhpHnGbw8hL9qpGM-BSUkQ"
+    spreadsheet = client.open_by_key(sheet_id)
+
+    sheet = spreadsheet.sheet1
+    data = sheet.get_all_records()
+    df = pd.DataFrame(data)
+    print(df.head(5))
+    return df
+
+# Checking the path to .json
+# import os
+# print(os.path.exists("credentials/forms-excel-service.json")) # returned false, so this is the wrong path
+# print(os.path.exists("../credentials/forms-excel-service.json")) # returned true, so this is the correct path

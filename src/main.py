4200d45f21251958e6a7b3bcbeed23cb82930082
Shalom Oyewusi <damolaso@hotmail.com>
2025-09-03 00:59:25 -0600

    testing for fetch_forms.py and clean_data.py are functional

--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
 from fetch_forms import fetch_form_responses
-
+from clean_data import clean_responses
 if __name__ == "__main__":
     sheet_id = "1nkrAqoIBc6Ze841PgVlElOhpHnGbw8hL9qpGM-BSUkQ"
     df = fetch_form_responses(sheet_id)
     print(df.head())
 
+df = fetch_form_responses(sheet_id)
+cleaned_df = clean_responses(df)
+print(cleaned_df.head())

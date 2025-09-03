d70a838f08830df585357efb6db7cd48a9937c1d
Shalom Oyewusi <damolaso@hotmail.com>
2025-09-03 01:20:07 -0600

    testing for fetch_forms.py, clean_data.py, and transform.py are functional

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from fetch_forms import fetch_form_responses
 from clean_data import clean_responses
+from transform import transform
 if __name__ == "__main__":
     sheet_id = "1nkrAqoIBc6Ze841PgVlElOhpHnGbw8hL9qpGM-BSUkQ"
     df = fetch_form_responses(sheet_id)
@@ -8,3 +9,10 @@ if __name__ == "__main__":
 df = fetch_form_responses(sheet_id)
 cleaned_df = clean_responses(df)
 print(cleaned_df.head())
+
+
+
+df = fetch_form_responses(sheet_id)
+cleaned_df = clean_responses(df)
+transformed_df = transform(cleaned_df)
+print(transformed_df.head())
